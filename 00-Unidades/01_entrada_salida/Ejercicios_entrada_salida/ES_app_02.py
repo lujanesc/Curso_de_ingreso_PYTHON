d255a6f79a7696475cd629d562f43c4d7cea6c89
lujan <lujiescalante@gmail.com>
2024-02-20 21:01:21 -0300

    resolvi ej ES

--- 00-Unidades/01_entrada_salida/Ejercicios_entrada_salida/ES_app_02.py
+++ 00-Unidades/01_entrada_salida/Ejercicios_entrada_salida/ES_app_02.py
@@ -5,8 +5,8 @@ from tkinter.simpledialog import askstring as prompt
 import customtkinter
 
 '''
-nombre:
-apellido:
+nombre: lujan
+apellido: escalante
 ---
 Ejercicio: entrada_salida_02
 ---
@@ -27,7 +27,8 @@ class App(customtkinter.CTk):
 
 
     def btn_mostrar_on_click(self):
-        pass
+        nombre=prompt(title="pregunta", prompt="¿cual es su nombre?")
+        alert(title="info", message=nombre)
         
         
     
--- 00-Unidades/01_entrada_salida/Ejercicios_entrada_salida/ES_app_03.py
+++ 00-Unidades/01_entrada_salida/Ejercicios_entrada_salida/ES_app_03.py
@@ -5,8 +5,8 @@ from tkinter.simpledialog import askstring as prompt
 import customtkinter
 
 '''
-nombre:
-apellido:
+nombre: lujan
+apellido: escalante
 ---
 Ejercicio: entrada_salida_03
 ---
@@ -34,7 +34,9 @@ class App(customtkinter.CTk):
 
 
     def btn_mostrar_on_click(self):
-        pass
+        nombre = self.txt_nombre.get()
+        saludo = "Hola " + nombre       
+        alert(title= "saludo", message=saludo)
                 
     
 if __name__ == "__main__":
